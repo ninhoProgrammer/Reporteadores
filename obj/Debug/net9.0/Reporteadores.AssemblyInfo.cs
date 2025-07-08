693cff9a5707115e5fb8b3036588e68b57f2fd5d
ninhoProgrammer <mariohm100293@gmail.com>
2025-07-08 13:33:38 -0600

    Update static web assets and dependencies
    - Updated fingerprint and integrity values for CSS and PDF assets in msbuild.Reporteadores.Microsoft.AspNetCore.StaticWebAssetEndpoints.props and msbuild.Reporteadores.Microsoft.AspNetCore.StaticWebAssets.props.
    - Changed asset filenames to reflect new fingerprints.
    - Updated package-lock.json and package.json to upgrade @tailwindcss/cli and related dependencies to version 4.1.11.
    - Updated input.output.css to reflect the new Tailwind CSS version.

--- obj/Debug/net9.0/Reporteadores.AssemblyInfo.cs
+++ obj/Debug/net9.0/Reporteadores.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Reporteadores")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d58194f9761664690f504171621539d8e7dd14f2")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0ce8c0c3472294b512f8c38f22b7da58d32c1a4d")]
 [assembly: System.Reflection.AssemblyProductAttribute("Reporteadores")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Reporteadores")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
